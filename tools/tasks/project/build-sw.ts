7cb384f1a271fb0cd3e0035c3a0ea8c11612aac0
Euan Goddard <euan.goddard@gmail.com>
2016-04-06 21:25:20 +0100

    Removed unused imports

--- tools/tasks/project/build-sw.ts
+++ tools/tasks/project/build-sw.ts
@@ -1,9 +1,7 @@
-import * as gulp from 'gulp';
 import {join} from 'path';
 import {APP_DEST} from '../../config';
 
 export = (callback: any) => {
-  let path = require('path');
   let swPrecache = require('sw-precache');
 
   swPrecache.write(join(APP_DEST, 'sw.js'), {
